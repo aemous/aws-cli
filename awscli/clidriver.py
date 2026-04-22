ab06ce669fd66e59e32d5910e68075d7294e0253
aemous <amoustafa0518@gmail.com>
2026-04-22 11:24:31 -0400

    Cleanup unnecessary changes.

--- awscli/clidriver.py
+++ awscli/clidriver.py
@@ -110,11 +110,10 @@ def main():
 def create_clidriver(args=None):
     import awscli.perf_timer as T
     debug = None
-    parsed_args = None
     if args is not None:
         parser = FirstPassGlobalArgParser()
-        parsed_args, _ = parser.parse_known_args(args)
-        debug = parsed_args.debug
+        args, _ = parser.parse_known_args(args)
+        debug = args.debug
     session = botocore.session.Session()
     _set_user_agent_for_session(session)
     # Replace the session's default emitter with a LazyInitEmitter
